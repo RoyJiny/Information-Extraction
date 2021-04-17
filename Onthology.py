48a9ce509b0de5792256c438361599b58a6cdad5
Roy Jiny <royjiny@gmail.com>
2021-04-17 10:52:56 +0300

    create onthology dict + inverse relations | dump to json file

--- Onthology.py
+++ Onthology.py
@@ -1,12 +1,38 @@
 import lxml.html
 import requests
+import json
 
 ONTHOLOGY_FILE_NAME = "onthology.nt"
+
+###
+# onthology film dictionary:
+# keys - film name
+# values - array of relations, each has a type and a "to" field
+###
+
+class Relation:
+    def __init__(self,relation_type,to):
+        self.relation_type = relation_type
+        self.to = to
+
+    def __str__(self):
+        return f"type: {self.relation_type}, to: {self.to}"
+    
+    def __repr__(self):
+        return f"type: {self.relation_type}, to: {self.to}"
+
+class OnthologyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Relation): 
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
 
 class Onthology:
     def __init__(self,URL):
         self.URL = URL
         self.film_list = []
+        self.film_onthology = {}
+        self.inverse_film_onthology = {}
 
     def collect_film_list(self):
         res = requests.get(self.URL)
@@ -20,16 +46,37 @@ class Onthology:
             except:
                 pass
 
+    def add_inverse_relation(self,film,relation_type,entity):
+        if not entity in self.inverse_film_onthology.keys():
+            self.inverse_film_onthology[entity] = []
+        for relation in self.inverse_film_onthology[entity]:
+            if relation.relation_type == relation_type:
+                relation.to.append(film)
+                return
+        new_relation = Relation(relation_type,film)
+        self.inverse_film_onthology[entity].append(new_relation)
+
     def collect_wiki_data_for_films(self):
         for film in self.film_list:
             url = f"https://en.wikipedia.org/wiki/{film.replace(' ','_')}"
-            # print(f"film: {film}")
             res = requests.get(url)
             doc = lxml.html.fromstring(res.content)
+            self.film_onthology[film] = []
             for tablerow in doc.xpath("//table[contains(@class,'infobox')]//tr//th[@class='infobox-label']"):
                 try:
                     label = tablerow.xpath(".//text()")[0]
                     value = tablerow.xpath("../td/a//text()") + tablerow.xpath("../td/text()") + tablerow.xpath("../td//li//text()")
-                    # print(label,value)
+                    relation = Relation(label,value)
+                    self.film_onthology[film].append(relation)
+                    for entity in value:
+                        self.add_inverse_relation(film,label,entity)
                 except:
                     pass
+
+    def create_onthology_file(self):
+        onthology = {
+            "direct film relations": self.film_onthology,
+            "inverse film relations": self.inverse_film_onthology
+        }
+        with open('onthology.nt', 'w') as onthology_file:
+            json.dump(onthology, onthology_file, cls=OnthologyEncoder)
